e4142a59ad63cab3a1c26d61771f6fed4d740302
Livio Lando <oboss@Livios-MacBook-Air.local>
2026-09-20 16:55:16 +0200

    modifiche mona

--- teoria_RMT/quad_monasson.py
+++ teoria_RMT/quad_monasson.py
@@ -32,30 +32,33 @@ s_guess = -1j / eta  # stima iniziale
 alpha = 0.2
 
 for z_real in lambda_vals:
-    z = z_real + 1j * eta
+    z = z_real - 1j * eta
     s = s_guess
+    if s_guess is None:
+        s = 1.0 / z
+    else:
+        s = s_guess
+
     for _ in range(300):
-        sum_term = np.sum(Gamma_k / (1.0 + s * Gamma_k)) / N
-        s_next = 1.0 / (sum_term - z)
+        sum_term = np.sum(Gamma_k / (1.0 - s * Gamma_k)) / N
+        s_next = 1.0 / (z-sum_term)
         s = (1.0 - alpha) * s + alpha * s_next
     
     #utilizziamo la s come una bnuova guess
     s_guess = s
-    rho_analitica.append(np.abs(np.imag(s)) / np.pi)
+    rho_analitica.append(np.imag(s) / np.pi)
 # plot
 plt.figure(figsize=(12, 7))
 
-plt.hist(eigvals, bins=200, density=True, alpha=0.5, color='blue', label="Simulazione ERM (N=1200)")
-plt.plot(eigvals, np.full_like(eigvals, 1e-2), '|', color='midnightblue', markersize=10, label="Autovalori discreti esatti")
+plt.hist(eigvals, bins=200, density=True, alpha=0.5, color='blue', label="Simulazione ERM (N=1500 100 realizzazioni)")
 plt.plot(lambda_vals, rho_analitica, 'r-', linewidth=2, label="Soluzione Analitica (Bulk)")
-plt.axvline(x=Gamma_k[0], color='k', linestyle='--', label=r"$\hat{\Gamma}(\pm 1)$ termodinamico")
+plt.axvline(x=Gamma_k[0], color='k', linestyle='--', label=r"$\hat{\Gamma}(\pm 1)$")
 #autovalore omesso dal bulk
-plt.axvline(gamma_hat_0, color='orange', linestyle='-.', label=r"$\hat{\Gamma}(0)$ (Autovalore estensivo)")
+plt.axvline(gamma_hat_0, color='orange', linestyle='-.', label=r"$\hat{\Gamma}(0)$ (Autovalore rimosso)")
 plt.yscale('log')
 plt.ylim(1e-2, 5e2) # Tagliamo il grafico per escludere il rumore numerico inferiore
 plt.xlabel(r"$\lambda$")
 plt.ylabel(r"$\rho(\lambda)$ (Scala Logaritmica)")
 plt.legend()
-plt.title("Confronto Spettro ERM: L=1 rivela i picchi discreti nel rumore")
 plt.grid(True, alpha=0.3)
 plt.show()
